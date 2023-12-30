047255be42a0514524d66d5d8dfdac99739481a0
ANKIT <ankitcloud2563@gmail.com>
2023-12-30 17:33:46 +0530

    adding index.js in routes directory to route different different things

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+//this is entry point of the routs
+// express router
+// /home
+//     / contact 
+//     / about
+//     / user / profile
+//     / user / edit 
+//     / user / create 
+
+const express = require('express');
+
+const router = express.Router();
