3073d87ee7fe8981c6ab0e3faa7d04ee7cd736f2
ANKIT <ankitcloud2563@gmail.com>
2024-01-03 00:31:05 +0530

    adding user profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,13 @@
 //going to control many users
 module.exports.profile = function (req, res) {
-   return res.end('<h1>User Profile</h1>')
+//    return res.end('<h1>User Profile</h1>')
+    return res.render('user_profile', {
+        title:'this is page of the profile '
+    });
 }
 module.exports.posting = function (req, res) {
-    return res.end('<h1>User post is here</h1>');
+    // return res.end('<h1>User post is here</h1>');
+    return res.render("user_posting", {
+      title: "this is page of the posting ",
+    });
 }
