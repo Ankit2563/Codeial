019dbe41bd9fb5391c03e7c0c33b46eb33e0d8ac
ANKIT <ankitcloud2563@gmail.com>
2024-01-02 23:41:47 +0530

    ejs file home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 module.exports.home = function (req, res) {
-    return res.end('<h1>Express is up</h1>')
+    // return res.end('<h1>Express is up</h1>')
+    return res.render('home', {
+        title: 'home'
+    })
 }
 //module.exports.actionName=function(req,res){}
