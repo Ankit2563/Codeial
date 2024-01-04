d6e7d893a37f2b99fd85a08982c2c4766b086dc0
ANKIT <ankitcloud2563@gmail.com>
2024-01-05 01:22:15 +0530

    updating the mongoose.js and the adding the cookie on the browser modify in the home_controller.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,16 @@
 /** @format */
 
+// require th library
 const mongoose = require("mongoose");
-
-// mongoose.connect("mongodb://localhost/codeial_development");
-// Replace this with your actual connection string
-const mongooseConnectionString = 'mongodb://localhost:27017/codeial_development';
-// mongoose.connect.mongooseConnectionString
-
+// connect to database
+mongoose.connect("mongodb://127.0.0.1:27017");
+// acquire the connection to check if it is successfull
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+//error
+db.on("error", console.error.bind(console, "error connecting to db"));
 
+// up and running the print the message
 db.once("open", function () {
-  console.log("Connected to Database :: MongoDB");
+  console.log("successfully connected to the database");
 });
-
-module.exports = db;
--- index.js
+++ index.js
@@ -1,23 +1,35 @@
+
+
 /** @format */
 const express = require("express");
 const app = express();
-const port = 8000;
+const cookieParser = require('cookie-parser');
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
 
+// Middleware
 app.use(express.static("./assets"));
 
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cookieParser());
+
+
 app.use(expressLayouts);
-// extract style and scripts from sub pages into the layout
+
+// View Engine Configuration
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
-// use express router
+// Routes
 app.use("/", require("./routes"));
 
-// set up the view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
+// Port Configuration
+const port = 8000;
 
 app.listen(port, function (err) {
   if (err) {
@@ -26,6 +38,82 @@ app.listen(port, function (err) {
 
   console.log(`Server is running on port: ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /** @format */
+// const express = require("express");
+// const app = express();
+// const cookieParser = require('cookie-parser')
+// const port = 8000;
+// const expressLayouts = require("express-ejs-layouts");
+// const db = require("./config/mongoose");
+
+
+// app.use(express.urlencoded());
+
+
+// app.use(cookieParser());
+
+
+// app.use(express.static("./assets"));
+
+// app.use(expressLayouts);
+// // extract style and scripts from sub pages into the layout
+// app.set("layout extractStyles", true);
+// app.set("layout extractScripts", true);
+
+// // use express router
+// app.use("/", require("./routes"));
+
+// // set up the view engine
+// app.set("view engine", "ejs");
+// app.set("views", "./views");
+
+// app.listen(port, function (err) {
+//   if (err) {
+//     console.log(`Error in running the server: ${err}`);
+//   }
+
+//   console.log(`Server is running on port: ${port}`);
+// });
 
 // /** @format */
 
