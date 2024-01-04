9a435bbfa033d377f6422fac2854d1ac9860455d
ANKIT <ankitcloud2563@gmail.com>
2024-01-05 03:32:07 +0530

    updating in the local branch

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,11 +2,35 @@
 const User = require('../models/user');//to use in creating sign Up
 
 
-module.exports.profile = function (req, res) {
-//    return res.end('<h1>User Profile</h1>')
-    return res.render('user_profile', {
-        title:'this is page of the profile '
-    });
+module.exports.profile = async function (req, res) {
+    //    return res.end('<h1>User Profile</h1>')
+    // now how to authenticate the profile
+    try {
+        if (req.cookies.user_id) {
+           
+        }
+        else {
+            return res.redirect('users/sign-in');
+        }
+
+    if (user) {
+      // Handle password check
+      if (user.password !== req.body.password) {
+        return res.redirect("back");
+      }
+
+      // Handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      // Handle user not found
+      return res.redirect("back");
+    }
+  } catch (error) {
+    console.error("Error in creating session:", error);
+    return res.redirect("back");
+  }
+    
 }
 module.exports.post= function (req, res) {
     // return res.end('<h1>User post is here</h1>');
