c920d694b79b7d15f3cfea456d8d1dd15e272258
ANKIT <ankitcloud2563@gmail.com>
2024-01-06 03:54:38 +0530

    done

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,46 +1,56 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+/** @format */
 
-const User = require('..models/user');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-//authentication using the passport
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-   
-},
-    function (email, passport, done) {
-        //find a user and establish the identity
-        User.findOne({ email: email }, function (err, user) {
-            if (err) {
-                console.log('Error in finding the user -->passport');
-                return done(err);
-            }
-            if (!user || user.passport != passport) {
-                console.log("Invalid Username/Password");
-                return done(null, false);//error,for authentication
-            }
-            return done(null, user);
-        });
-        
+const User = require("../models/user");
+
+// authentication using the passport
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    async function (email, password, done) {
+      try {
+        // find a user and establish the identity
+        const user = await User.findOne({ email: email });
+
+        if (!user || user.password !== password) {
+          console.log("Invalid Username/Password");
+          return done(null, false); // error, for authentication
+        }
+
+        return done(null, user);
+      } catch (err) {
+        console.log("Error in finding the user --> passport", err);
+        return done(err);
+      }
     }
-));
+  )
+);
 
 // serialize the user to decide which key is to be kept in the cookies
 passport.serializeUser(function (user, done) {
-    // to do in encrypted format,null is for the no error
-    done(null, user.id);
+  // to do in encrypted format, null is for no error
+  done(null, user.id);
 });
 
+// deserializing the user from the key in the cookies
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
 
-//deserializing the user from the key in the cookies
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        if (err) {
-            console.log("Error in finding the user -->passport");
-            return done(err);
-        }
-        return done(null, user);
-    })
+    if (!user) {
+      console.log("User not found");
+      return done(null, false);
+    }
+
+    return done(null, user);
+  } catch (err) {
+    console.log("Error in finding the user --> passport", err);
+    return done(err);
+  }
 });
 
-module.exports=passport
+module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users_controller');
+const passport = require('passport');
 
 router.get('/profile', userController.profile);
 router.get('/post', userController.post);
@@ -10,4 +11,11 @@ router.get('/sign-in', userController.signIn);
 
 // router.get('/create', userController.create);
 router.post('/create', userController.create);
+
+// use passport as a middleware to authenticate
+router.post('/create-sessions', passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'},
+
+), userController.createSession);
 module.exports = router;
